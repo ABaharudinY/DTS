ca04284541a945c486cfac62208bcf13d64fbd99
AbaharudinY <baharudinyusuf99023@gmail.com>
2019-07-12 16:46:51 +0800

    Pertemuan 9

--- Pertemuan 9/Pertemuan_9A/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Pertemuan 9/Pertemuan_9A/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText e1,e2,e3;
+    TextView t1,t2,t3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        e1 = findViewById(R.id.edit1);
+        e2 = findViewById(R.id.edit2);
+        e3 = findViewById(R.id.edit3);
+        t1 = findViewById(R.id.text5);
+        t2 = findViewById(R.id.text6);
+        t3 = findViewById(R.id.text7);
+
+    }
+
+    public  void  sehasil(View view){
+        t1.setText("NIM adalah "+e1.getText().toString());
+        t2.setText("NAMA adalah "+e2.getText().toString());
+        t3.setText("KAMPUS adalah "+e3.getText().toString());
     }
 }
--- Pertemuan 9/Pertemuan_9B/app/src/main/java/com/example/mainactivity/MainActivity.java
+++ Pertemuan 9/Pertemuan_9B/app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.mainactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private ListView LvItem;
+    private String[] namanegara = new String[]{
+            "Indonesia","Malaysia","Singapore",
+            "Italia","Inggris","Belanda",
+            "Argentina","Chile",
+            "Mesir","Uganda"
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("ListView Sederhana");
+
+
+        LvItem = (ListView) findViewById(R.id.listNama);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>( getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                namanegara);
+
+        LvItem.setAdapter(adapter);
+        LvItem.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+             public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                 Toast.makeText( getApplicationContext(), "Memilih : "+namanegara[position],
+                 Toast.LENGTH_LONG).show();
+             }
+        });
     }
 }
